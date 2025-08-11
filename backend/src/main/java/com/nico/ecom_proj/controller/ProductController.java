05b14fed14a17867720c9877669a6d84864bdacc
JNico <nicoolano43@gmail.com>
2025-08-11 22:15:15 +0800

    initialize project structure with basic configurations and product management functionality

---
+++ backend/src/main/java/com/nico/ecom_proj/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.nico.ecom_proj.controller;
+
+import com.nico.ecom_proj.model.Product;
+import com.nico.ecom_proj.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @RequestMapping("/")
+    public String greet() {
+        return "Hello World";
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return service.getAllProducts();
+    }
+
+}
---
+++ backend/src/main/java/com/nico/ecom_proj/service/ProductService.java
@@ -0,0 +1,21 @@
+package com.nico.ecom_proj.service;
+
+import com.nico.ecom_proj.model.Product;
+import com.nico.ecom_proj.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo repo;
+
+    public List<Product> getAllProducts() {
+
+        return repo.findAll();
+
+    }
+}
