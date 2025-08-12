a2ca9c94df08824eebbd905a3ccded4141b604e6
JNico <nicoolano43@gmail.com>
2025-08-12 17:55:15 +0800

    feat: implement product image upload and retrieval functionality, update Product model with new fields

--- backend/src/main/java/com/nico/ecom_proj/controller/ProductController.java
+++ backend/src/main/java/com/nico/ecom_proj/controller/ProductController.java
@@ -4,8 +4,10 @@ import com.nico.ecom_proj.model.Product;
 import com.nico.ecom_proj.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -23,7 +25,7 @@ public class ProductController {
         return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
     }
 
-    @GetMapping("/products/{id}")
+    @GetMapping("/product/{id}")
     public ResponseEntity<Product> getProduct(@PathVariable int id) {
 
         Product product = service.getProductById(id);
@@ -31,6 +33,30 @@ public class ProductController {
             return new ResponseEntity<>(product, HttpStatus.OK);
         else
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product,
+                                        @RequestPart MultipartFile imageFile) {
+        System.out.print(product);
+        try {
+            Product product1 = service.addProduct(product, imageFile);
+            return new ResponseEntity<>(product1, HttpStatus.CREATED);
+        } catch(Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/product/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId) {
+
+        Product product = service.getProductById(productId);
+        byte[] imageFile = product.getImageDate();
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(product.getImageType()))
+                .body(imageFile);
+
     }
 
 }
--- backend/src/main/java/com/nico/ecom_proj/service/ProductService.java
+++ backend/src/main/java/com/nico/ecom_proj/service/ProductService.java
@@ -4,7 +4,9 @@ import com.nico.ecom_proj.model.Product;
 import com.nico.ecom_proj.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -22,5 +24,14 @@ public class ProductService {
     public Product getProductById(int id) {
         return repo.findById(id)
                 .orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        // get image details and set to product object
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageDate(imageFile.getBytes());
+
+        return repo.save(product);
     }
 }
